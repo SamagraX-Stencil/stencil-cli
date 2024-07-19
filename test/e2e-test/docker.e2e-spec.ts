f51d2514205e1bcce4b905cdce637ddc46594ab7
Savio629 <diassavio629@gmail.com>
2024-07-19 22:12:41 +0530

    fix: updated test cases for docker command

--- test/e2e-test/docker.e2e-spec.ts
+++ test/e2e-test/docker.e2e-spec.ts
@@ -20,7 +20,7 @@ describe('Stencil cli e2e Test - DOCKER command', () => {
     });
   });
 
-  it('should run "docker" command with valid services and check if the folder is generated', (done) => {
+  it('should run "docker" command with tooling services and check if the folder is generated', (done) => {
     const services = ['monitoringService', 'logging', 'temporal'];
     const child = exec(`npx stencil docker ${services.join(' ')}`, (error, stdout, stderr) => {
       if (error) {
@@ -41,9 +41,9 @@ describe('Stencil cli e2e Test - DOCKER command', () => {
       child.kill();
       done();
     });
-  }, 15000);
+  },10000 );
 
-  it('should run "docker" command with valid services and check if the docker compose is updated', (done) => {
+  it('should run "docker" command with adhoc services and check if the docker compose is updated', (done) => {
     const services = ['postgres', 'hasura'];
     const child = exec(`npx stencil docker ${services.join(' ')}`, (error, stdout, stderr) => {
       if (error) {
@@ -67,9 +67,56 @@ describe('Stencil cli e2e Test - DOCKER command', () => {
 
       rmSync(dockerComposeFile, { force: true });
       rmSync(join(process.cwd(),'.env'), { recursive: true, force: true });
+      rmSync(join(process.cwd(),'docker-start.sh'), { recursive: true, force: true });
 
       child.kill();
       done();
     });
-  }, 10000);
+  }, );
+
+  it('should throw error for "docker" command with wrong path flag', (done) => {
+    const services = ['postgres', 'hasura'];
+    const child = exec(`npx stencil docker ${services.join(' ')} --path`, (error, stdout, stderr) => {
+      services.forEach((service) => {
+        expect(stderr).toContain(`Schematic input does not validate against the Schema: {"name":"${service}","language":"ts","path":true}`);
+        expect(stderr).toContain(`Data path "/path" must be string.`);
+      });
+      
+      child.kill();
+      done();
+    });
+  }, );
+
+  it('should run "docker" command with path flag', (done) => {
+    const services = ['postgres', 'hasura'];
+    const child = exec(`npx stencil docker ${services.join(' ')} --path temp`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        child.kill();
+        done(error);
+        return;
+      }
+      services.forEach((service) => {
+        expect(stderr).not.toContain(`Invalid schematic "${service}". Please, ensure that "${service}" exists in this collection.`);
+      });
+
+      const dockerComposeFile = join(process.cwd(),'temp', 'docker-compose.yml');
+      expect(existsSync(dockerComposeFile)).toBe(true);
+
+      const dockerComposeContent = readFileSync(dockerComposeFile, 'utf8');
+      const dockerComposeConfig = yaml.load(dockerComposeContent) as any;
+
+      expect(dockerComposeConfig.services.postgres).toBeDefined();
+      expect(dockerComposeConfig.services.hasura).toBeDefined();
+
+      rmSync(dockerComposeFile, { force: true });
+      rmSync(join(process.cwd(),'temp','.env'), { recursive: true, force: true });
+      rmSync(join(process.cwd(),'docker-start.sh'), { recursive: true, force: true });
+      rmSync(join(process.cwd(), 'temp'), { recursive: true, force: true });
+
+      child.kill();
+      done();
+    });
+  }, );
+
 });
