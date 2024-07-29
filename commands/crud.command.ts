3afd0917a3b2c02bda55135138cc5f9c774630b6
Savio629 <diassavio629@gmail.com>
2024-07-29 20:30:21 +0530

    fix: updated a condition of crud command

--- commands/crud.command.ts
+++ commands/crud.command.ts
@@ -9,6 +9,10 @@ export class CrudCommand extends AbstractCommand {
       .alias('cr')
       .description('Generate CRUD API for specified models.')
       .action(async (inputArgs: string[] = []) => {
+        if (inputArgs.length === 0) {
+          console.error('No model provided. Please specify a model or use "*" to generate all models.');
+          return;
+        }
         const inputs: Input[] = inputArgs.map(arg => ({ name: arg, value: arg }));
         await this.action.handle(inputs);
       });
